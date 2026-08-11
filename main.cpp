00abe01b39c54c3279492008ee7b58d70dd88d7e
Ishan Kulkarni <ikulkarni8@gatech.edu>
2026-08-11 01:13:04 -0400

    Moving dependencies to separate folder

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #define GL_SILENCE_DEPRECATION
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include "cyTriMesh.h"
-#include "cyGL.h"
-#include "cyMatrix.h"
+#include "cy/cyTriMesh.h"
+#include "cy/cyGL.h"
+#include "cy/cyMatrix.h"
 #include "lodepng.h"
 #include <iostream>
 #include <iterator>
